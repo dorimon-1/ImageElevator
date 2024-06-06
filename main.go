17ebcd0a6e3afa5752769ba88287700262076ddd
KJone1 <davidyahin@protonmail.com>
2024-06-06 17:37:00 +0300

    Update to match recent changes

--- main.go
+++ main.go
@@ -3,25 +3,17 @@ package main
 import (
 	"log"
 
+	"github.com/Kjone1/imageElevator/endpoints"
 	"github.com/gin-gonic/gin"
 )
 
-func healthEndpoint(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
 func main() {
 	server := gin.Default()
-	client, err := ftpConnect()
-	if err != nil {
-		log.Fatalln(err)
-	}
 
 	v1 := server.Group("/v1")
 	{
-		v1.GET("/ping", healthEndpoint)
-		v1.GET("/list", client.ftpListEndpoint)
+		v1.GET("/ping", endpoints.Health)
+		v1.GET("/sync", endpoints.FtpSync)
 	}
 	server.Run()
 }
